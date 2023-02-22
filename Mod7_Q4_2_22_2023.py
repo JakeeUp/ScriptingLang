46b5d5b78feddce1cce48dee92748b9da9456e84
Jacob060701 <fjacob31@yahoo.com>
2023-02-22 13:13:15 -0600

    2/22/2023

---
+++ Mod7_Q4_2_22_2023.py
@@ -0,0 +1,7 @@
+#question 4 2/22/23
+
+cube_edge = [1, 5, 3, 10]
+
+for edge in cube_edge:
+  surface_area = 6 * edge ** 2
+  print("The surface area of a cube with an edge length of {0} is {1}".format(edge,surface_area))
