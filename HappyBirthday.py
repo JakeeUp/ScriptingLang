2a08c0ae0a9d83f11e8ee5bf2853803512052529
Jacob060701 <fjacob31@yahoo.com>
2023-03-06 12:06:14 -0600

    Homework done

---
+++ HappyBirthday.py
@@ -0,0 +1,68 @@
+#using tkinter draw a balloon 
+
+
+
+
+import winsound
+import time
+
+from tkinter import *
+
+  
+root = Tk()
+canvas = Canvas(root, width=200, height=200)
+canvas.pack()
+root.geometry("200x200") 
+  
+root.title("HAPPY BIRTHDAY FRANK") 
+  
+
+# draw the balloon shape 
+canvas.create_oval(50, 10, 150, 110, fill="red")  # balloon body 
+canvas.create_line(100, 110, 100, 200) # string of the balloon 
+
+
+root.mainloop()
+
+
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(3000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,1000)
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(3000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,1000)
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,500) 
+time.sleep(0.5) 
+winsound.Beep(3000,500) 
+time.sleep(0.5) 
+winsound.Beep(2000,500) 
+time.sleep(0.5) 
+winsound.Beep(1000,1000)
+
+
