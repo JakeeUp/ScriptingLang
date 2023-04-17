3f9e312f0f69b2cb06193a8790b0d9ae9f5f3140
Jacob060701 <fjacob31@yahoo.com>
2023-04-17 12:42:56 -0500

    yes

---
+++ ImportingTest.py
@@ -0,0 +1,32 @@
+# from mymodule import*
+# PG = personalizedGreeting
+# PG("Jacob","Fernandez")
+
+# from math import*
+# print(sqrt(64))
+# print(ceil(13.3))
+# print(floor(13.3))
+
+# import datetime
+# print(datetime.date.today())
+# print(datetime.datetime.now())
+# print(dir(datetime))
+
+# import calendar
+# print(calendar.isleap(2020))
+# print(calendar.leapdays(2000,2023))
+# print(calendar.month(2020, 10))
+
+# import platform
+# print(platform.system())
+# print(platform.architecture())
+# print(platform.processor())
+# print(dir(platform))
+
+# import os
+# print(os.getcwd())
+# print(os.listdir())
+
+# import random
+# myList = [55,28,95,'Jacob','Fernandez',50,'Apple']
+# print(random.choice(myList))
---
+++ mymodule.py
@@ -0,0 +1,6 @@
+def personalizedGreeting(FName,LName):
+    print(f"hello {FName} {LName}")
+
+def applyRaise(pay):
+    pay*=1.05
+    print(f"New income with raise = {pay}")
